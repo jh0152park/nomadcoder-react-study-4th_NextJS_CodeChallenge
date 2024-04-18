8336a9a2f3f44daed0be89e039e2a19b0eac6a54
jh0152park <jh0152park@naver.com>
2024-04-18 17:30:28 +0900

    done

--- mutation/app/page.tsx
+++ mutation/app/page.tsx
@@ -1,4 +1,28 @@
+"use client";
+
+import useSWR from "swr";
+
+interface IResponse {
+    url: string;
+    isLiked: boolean;
+}
+
+async function fetcher() {
+    const API_URL = "https://dogs-api.nomadcoders.workers.dev";
+    return fetch(API_URL).then((res) => res.json());
+}
+
 export default function Home() {
+    const { data, mutate } = useSWR("dog_video", fetcher);
+
+    function onDogClick() {
+        mutate();
+    }
+
+    function toggleLike() {
+        mutate({ ...data, isLiked: !data?.isLiked }, false);
+    }
+
     return (
         <main className="flex justify-center items-start w-full h-screen bg-[url(https://assets.coingecko.com/coins/images/33898/large/msg-4035486419-2265.jpg)] flex-col px-[20%] py-[15%] ">
             <span className="mb-10 text-4xl font-bold">Popo.tv</span>
@@ -12,14 +36,20 @@ export default function Home() {
                         width: "500px",
                         height: "350px",
                     }}
-                    src="https://random.dog/4eee5dd0-189c-45b0-83a5-c63d31c11242.mp4"
+                    src={data?.url}
                 ></video>
                 <div className="flex items-center justify-center w-full h-[50px] gap-1">
-                    <button className="w-full h-full bg-white rounded-lg hover:cursor-pointer hover:bg-[#f3f3f3] transition-all">
+                    <button
+                        className="w-full h-full bg-white rounded-lg hover:cursor-pointer hover:bg-[#f3f3f3] transition-all"
+                        onClick={onDogClick}
+                    >
                         🐶
                     </button>
-                    <button className="w-full h-full transition-all rounded-lg bg-sky-400 hover:cursor-pointer hover:bg-sky-500">
-                        ❤️
+                    <button
+                        className="w-full h-full transition-all rounded-lg bg-sky-400 hover:cursor-pointer hover:bg-sky-500"
+                        onClick={toggleLike}
+                    >
+                        {data?.isLiked ? "😻" : "😿"}
                     </button>
                 </div>
             </section>
