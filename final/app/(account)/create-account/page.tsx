90c249bb111af342f9dbd8c8d9d5ebd91c69c476
jh0152park <jh0152park@naver.com>
2024-04-24 22:57:40 +0900

    create accout done

--- final/app/(account)/create-account/page.tsx
+++ final/app/(account)/create-account/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Input from "@/components/input";
 import LoadingButton from "@/components/loading-button";
 import Image from "next/image";
+import { useFormState } from "react-dom";
+import { CreateAccount } from "./action";
 
-export default function CreateAccount() {
+export default function CreateAccountPage() {
+    const [state, trigger] = useFormState(CreateAccount, null);
+
     return (
-        <div className="flex flex-col items-center justify-start w-full h-full gap-10">
+        <div className="flex flex-col items-center justify-start w-full h-full gap-10 px-10">
             <Image
                 src="/image/logo.png"
                 alt="logo"
@@ -17,22 +23,33 @@ export default function CreateAccount() {
                 }}
             />
 
-            <form className="flex flex-col gap-3 ">
-                <Input name="email" type="email" placeholder="Email" required />
+            <form action={trigger} className="flex flex-col gap-3 ">
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    errors={state?.fieldErrors.email}
+                />
                 <Input
                     name="username"
                     type="text"
                     placeholder="Username"
                     required
+                    errors={state?.fieldErrors.username}
                 />
                 <Input
                     name="password"
-                    type="text"
+                    type="password"
                     placeholder="Password"
                     required
+                    errors={state?.fieldErrors.password}
                 />
                 <LoadingButton name="Create" />
             </form>
+            <span className="text-sm text-center w-full -mt-6">
+                Will be redirect to login page if account created 😎
+            </span>
         </div>
     );
 }
