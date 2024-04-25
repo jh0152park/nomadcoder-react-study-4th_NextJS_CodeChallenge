c1155ca428bfead159f9cfdde0305ccd64104e90
jh0152park <jh0152park@naver.com>
2024-04-25 17:48:55 +0900

    added write post page

--- final/app/(screens)/write/page.tsx
+++ final/app/(screens)/write/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import TextArea from "@/components/text-area";
+import LoadingButton from "@/components/loading-button";
+import BackButton from "@/components/profile/back-button";
+import { useFormState } from "react-dom";
+import { UploadPost } from "./action";
+
 export default function Write() {
-    return <h1>Write Page</h1>;
+    const [_, trigger] = useFormState(UploadPost, null);
+
+    return (
+        <div className="w-full max-w-[430px] h-full flex flex-col items-start justify-start p-5">
+            <BackButton />
+
+            <span className="absolute text-xl font-bold -translate-x-1/2 left-1/2 top-5">
+                Write threads
+            </span>
+
+            <form action={trigger} className="flex flex-col w-full gap-3 mt-14">
+                <TextArea
+                    name="post"
+                    h="150"
+                    placeholder="What is happening? 😎 🌈 ✨ 👻 🍩"
+                    required
+                />
+                <LoadingButton name="Post" />
+            </form>
+        </div>
+    );
 }
--- final/components/text-area.tsx
+++ final/components/text-area.tsx
@@ -2,11 +2,13 @@ import { InputHTMLAttributes } from "react";
 
 interface IInput {
     name: string;
+    h?: string;
     errors?: string[];
 }
 
 export default function TextArea({
     name,
+    h = "80",
     errors = [],
     ...extraProps
 }: IInput & InputHTMLAttributes<HTMLTextAreaElement>) {
@@ -15,13 +17,16 @@ export default function TextArea({
             <textarea
                 name={name}
                 {...extraProps}
-                className="w-[390px] px-3 py-2 h-20 rounded-lg bg-black border text-[ghostwhite] placeholder:text-neutral-500 active:outline-none focus:outline-none font-semibold placeholder:font-semibold resize-none"
+                className="w-[390px] px-3 py-2 rounded-lg bg-black border text-[ghostwhite] placeholder:text-neutral-500 active:outline-none focus:outline-none font-semibold placeholder:font-semibold resize-none"
+                style={{
+                    height: `${h}px`,
+                }}
             />
-            {errors.map((error, index) => (
+            {/* {errors.map((error, index) => (
                 <span key={index} className="font-medium text-red-500">
                     {error}
                 </span>
-            ))}
+            ))} */}
         </div>
     );
 }
