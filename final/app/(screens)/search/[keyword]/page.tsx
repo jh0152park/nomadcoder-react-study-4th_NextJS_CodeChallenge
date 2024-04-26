c1e1eae511df431ab10916fc828d1fa0a88f8477
jh0152park <jh0152park@naver.com>
2024-04-27 03:09:37 +0900

    fixed search result layout

--- final/app/(screens)/search/[keyword]/page.tsx
+++ final/app/(screens)/search/[keyword]/page.tsx
@@ -12,14 +12,16 @@ export default async function SearchDetail({
     const posts = await SearchPost(decodeKeyword);
 
     return (
-        <div className="w-full max-w-[430px] h-full flex flex-col items-start justify-start  ">
-            <div className="pt-5 pl-5 mb-14">
-                <BackButton />
+        <div className="w-full max-w-[430px] h-full flex flex-col items-start justify-start pt-20 ">
+            <div className="fixed top-0 flex items-center max-w-[430px] justify-center w-full bg-black -translate-x-1/2 left-1/2 border border-neutral-400 border-t-0 border-b-0 h-[70px]">
+                <div className="left-5 top-5 absolute">
+                    <BackButton />
+                </div>
+                <span className="absolute text-xl font-bold -translate-x-1/2 left-1/2 top-5">
+                    Search for {decodeKeyword}
+                </span>
             </div>
 
-            <span className="absolute text-xl font-bold -translate-x-1/2 left-1/2 top-5">
-                Search for {decodeKeyword}
-            </span>
             {posts.map((post) => (
                 <PostSummary key={post.id} {...post} />
             ))}
