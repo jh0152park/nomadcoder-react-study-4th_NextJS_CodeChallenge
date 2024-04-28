464495c84bcac2316f47dfca726bf50c485fdb78
jh0152park <jh0152park@naver.com>
2024-04-28 17:00:10 +0900

    added comment feature

--- final/app/(screens)/tweet/[id]/action.ts
+++ final/app/(screens)/tweet/[id]/action.ts
@@ -14,3 +14,43 @@ export async function IsExistPost(id: number) {
 
     return Boolean(post);
 }
+
+export async function getPost(id: number) {
+    return await PRISMA_DB.post.findUnique({
+        where: {
+            id: +id,
+        },
+        select: {
+            id: true,
+            like: true,
+            userId: true,
+            payload: true,
+            comment: true,
+            user: {
+                select: {
+                    id: true,
+                    username: true,
+                    profile_image: true,
+                },
+            },
+        },
+    });
+}
+
+export async function AddComment(
+    id: number,
+    prevComment: string[],
+    newComment: string
+) {
+    return await PRISMA_DB.post.update({
+        where: {
+            id: +id,
+        },
+        data: {
+            comment: [...prevComment, newComment],
+        },
+        select: {
+            comment: true,
+        },
+    });
+}
--- final/app/(screens)/tweet/[id]/page.tsx
+++ final/app/(screens)/tweet/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from "next/navigation";
-import { IsExistPost } from "./action";
+import { IsExistPost, getPost } from "./action";
 import BackButton from "@/components/profile/back-button";
+import getSession from "@/lib/session/get-session";
+import PostSummary from "@/components/post/post-summary";
+import Comment from "@/components/post/comment/comment";
 
 export default async function TweetDetail({
     params,
@@ -8,6 +11,8 @@ export default async function TweetDetail({
     params: { id: number };
 }) {
     const id = params.id;
+    const session = await getSession();
+
     if (isNaN(id)) {
         notFound();
     }
@@ -16,11 +21,17 @@ export default async function TweetDetail({
         notFound();
     }
 
-    return (
-        <div className="w-full max-w-[430px] h-full flex flex-col items-start justify-start p-5 ">
-            <BackButton />
+    const post = await getPost(id);
 
-            <span className="absolute text-xl font-bold -translate-x-1/2 left-1/2 top-5"></span>
+    return (
+        <div className="w-full max-w-[430px] h-screen flex flex-col items-start justify-start pb-16">
+            <div className="pt-5 pl-5 mb-4">
+                <BackButton />
+            </div>
+            <PostSummary {...post!} sessionId={session.id!} />
+            <div className="w-full h-full overflow-y-scroll p-5">
+                <Comment id={id} comment={post?.comment!} />
+            </div>
         </div>
     );
 }
